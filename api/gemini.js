1621f4e7b7d8c5d21a72102623809818ac3ed2c5
3025404 <s3025404@st.shiga-u.ac.jp>
2025-05-26 19:10:02 +0900

    v1-beta

--- api/gemini.js
+++ api/gemini.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1/models/chat-bison-001:generateContent?key=
+      `https://generativelanguage.googleapis.com/v1beta/models/chat-bison-001:generateContent?key=
       ${GEMINI_API_KEY}`, {
       method: 'POST',
       headers: {
